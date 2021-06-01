31886c49d93982298d77de9493b0bb55eee2ace4
Luiz Lobo <luizfpll@gmail.com>
2021-06-01 18:19:08 -0300

    moving pydantic schemas

--- apis/models.py
+++ apis/models.py
@@ -1,6 +1,6 @@
 # File with pydantic and db models
 from sqlalchemy import String,Integer,Column,Float,Boolean
-from db import BaseDbModel
+from apis.db import BaseDbModel
 from pydantic import BaseModel,HttpUrl
 from pydantic.typing import Optional,Set
 
@@ -25,27 +25,4 @@ class UrlDbModel(BaseDbModel):
     is_arabic =  Column(Boolean,nullable=True)
     number_of_subpaths =  Column(Integer,nullable=True)
     related_original_url = Column(Boolean,nullable=True)
-
-# Pydantic Models
-class UrlBase(BaseModel):
-    id: int
-    url : str
-
-    class Config:
-        orm_mode = True
-
-
-class Url(UrlBase):
-    appearances: Optional[int]
-    domain:  Optional[str]
-    path:  Optional[str]
-    is_file:  Optional[bool]
-    is_arabic:  Optional[bool]
-    last_path_length:  Optional[int]
-    percent_of_letters_path:  Optional[float]
-    percent_of_numbers_path:  Optional[float]
-    path_length:  Optional[int]
-    full_lengh:  Optional[int]
-    number_of_subpaths:  Optional[int]
-    related_original_url: Optional[bool]
 
