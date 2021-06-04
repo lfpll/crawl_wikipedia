21afbe33ed8ac77fbe9fdcabbb298e7eaf344a2e
Luiz Lobo <luizfpll@gmail.com>
2021-06-04 15:06:25 -0300

    fixing missing columns on creating data

--- apis/crud.py
+++ apis/crud.py
@@ -1,21 +1,24 @@
 from apis.models import UrlDbModel
-from apis.schemas import UrlBase,Url
+from apis.schemas import Url
 from sqlalchemy.dialects.postgresql import insert
 from pydantic import HttpUrl
 from pydantic.typing import List
 from sqlalchemy.orm import Session
-from sqlalchemy import update
+from apis.ml_model.model import AppearancesModel
 
+model_obj = AppearancesModel() 
 
 def create_url(db: Session,url:Url):
-    new_url = UrlDbModel(**dict(url))
+    new_cols_objs = model_obj.add_columns(url.url).to_dict(orient='records')[0]
+    new_cols_objs['url'] = url.url
+    new_url = UrlDbModel(**new_cols_objs)
     db.add(new_url)
     db.commit()
     db.refresh(new_url)
     return new_url
 
 def get_url(db:Session, url:HttpUrl):
-    return db.query(UrlDbModel.url,UrlDbModel.id,UrlDbModel.appearances,UrlDbModel.domain,UrlDbModel.path,UrlDbModel.is_file,
+    return db.query(UrlDbModel.url,UrlDbModel.id,UrlDbModel.appearances,UrlDbModel.is_file,
                     UrlDbModel.is_arabic,UrlDbModel.last_path_length,UrlDbModel.percent_of_letters_path,UrlDbModel.percent_of_numbers_path,
                     UrlDbModel.path_length,UrlDbModel.full_lengh,UrlDbModel.number_of_subpaths,UrlDbModel.related_original_url).where(UrlDbModel.url == url).first()
 
